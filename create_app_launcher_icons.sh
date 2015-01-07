323e457f709f95c6409db59ed8f802d3ee80baea
Tim Kelly <austimkelly@gmail.com>
2015-01-07 11:08:21 -0600

    updated sizes for xcode 6.1

--- create_app_launcher_icons.sh
+++ create_app_launcher_icons.sh
@@ -48,13 +48,15 @@ mkdir -p $OUTPUT_DIR/iPad/iTunesArtwork
 #iOS App Icons - These are based on XCode 6.1 App Icon sizes for iOS 6.1 and greater.
 echo ==== Creating iPhone Icons ====
 #convert $INPUT_IMAGE -resize 40x40 $IPHONE_OUT_DIR/Icon-40.png
-convert $INPUT_IMAGE -resize 80x80 $IPHONE_OUT_DIR/Icon-40@2x.png
-convert $INPUT_IMAGE -resize 120x120 $IPHONE_OUT_DIR/Icon-40@3x.png
-convert $INPUT_IMAGE -resize 57x57 $IPHONE_OUT_DIR/Icon-57.png
-convert $INPUT_IMAGE -resize 114x114 $IPHONE_OUT_DIR/Icon-57@2x.png
-convert $INPUT_IMAGE -resize 29x29 $IPHONE_OUT_DIR/Icon-29.png
-convert $INPUT_IMAGE -resize 58x58 $IPHONE_OUT_DIR/Icon-29@2x.png
-convert $INPUT_IMAGE -resize 87x87 $IPHONE_OUT_DIR/Icon-29@3x.png
+convert $INPUT_IMAGE -resize 120x120 $IPHONE_OUT_DIR/iPhone-Icon-60@2x.png
+convert $INPUT_IMAGE -resize 180x180 $IPHONE_OUT_DIR/iPhone-Icon-60@3x.png
+convert $INPUT_IMAGE -resize 80x80 $IPHONE_OUT_DIR/iPhone-Icon-40@2x.png
+convert $INPUT_IMAGE -resize 120x120 $IPHONE_OUT_DIR/iPhone-Icon-40@3x.png
+convert $INPUT_IMAGE -resize 57x57 $IPHONE_OUT_DIR/iPhone-Icon-57.png
+convert $INPUT_IMAGE -resize 114x114 $IPHONE_OUT_DIR/iPhone-Icon-57@2x.png
+convert $INPUT_IMAGE -resize 29x29 $IPHONE_OUT_DIR/iPhone-Icon-29.png
+convert $INPUT_IMAGE -resize 58x58 $IPHONE_OUT_DIR/iPhone-Icon-29@2x.png
+convert $INPUT_IMAGE -resize 87x87 $IPHONE_OUT_DIR/iPhone-Icon-29@3x.png
 
 convert $INPUT_IMAGE -resize 1024x1024 $OUTPUT_DIR/iPhone/iTunesArtwork/iTunesArtwork@2x
 convert $INPUT_IMAGE -resize 512x512 $OUTPUT_DIR/iPhone/iTunesArtwork/iTunesArtwork
@@ -65,16 +67,16 @@ echo ==== Creating iPad Icons ====
 convert $INPUT_IMAGE -resize 1024x1024 $OUTPUT_DIR/iPad/iTunesArtwork/iTunesArtwork@2x
 convert $INPUT_IMAGE -resize 512x512 $OUTPUT_DIR/iPad/iTunesArtwork/iTunesArtwork
 
-convert $INPUT_IMAGE -resize 29x29 $IPAD_OUT_DIR/Icon-29.png
-convert $INPUT_IMAGE -resize 58x58 $IPAD_OUT_DIR/Icon-29@2x.png
-convert $INPUT_IMAGE -resize 40x40 $IPAD_OUT_DIR/Icon-40.png
-convert $INPUT_IMAGE -resize 80x80 $IPAD_OUT_DIR/Icon-40@2x.png
-convert $INPUT_IMAGE -resize 50x50 $IPAD_OUT_DIR/Icon-50.png
-convert $INPUT_IMAGE -resize 100x100 $IPAD_OUT_DIR/Icon-50@2x.png
-convert $INPUT_IMAGE -resize 72x72 $IPAD_OUT_DIR/Icon-72.png
-convert $INPUT_IMAGE -resize 144x144 $IPAD_OUT_DIR/Icon-72@2x.png
-convert $INPUT_IMAGE -resize 76x76 $IPAD_OUT_DIR/Icon-76.png
-convert $INPUT_IMAGE -resize 152x152 $IPAD_OUT_DIR/Icon-76@2x.png
+convert $INPUT_IMAGE -resize 29x29 $IPAD_OUT_DIR/iPad-Icon-29.png
+convert $INPUT_IMAGE -resize 58x58 $IPAD_OUT_DIR/iPad-Icon-29@2x.png
+convert $INPUT_IMAGE -resize 40x40 $IPAD_OUT_DIR/iPad-Icon-40.png
+convert $INPUT_IMAGE -resize 80x80 $IPAD_OUT_DIR/iPad-Icon-40@2x.png
+convert $INPUT_IMAGE -resize 50x50 $IPAD_OUT_DIR/iPad-Icon-50.png
+convert $INPUT_IMAGE -resize 100x100 $IPAD_OUT_DIR/iPad-Icon-50@2x.png
+convert $INPUT_IMAGE -resize 72x72 $IPAD_OUT_DIR/iPad-Icon-72.png
+convert $INPUT_IMAGE -resize 144x144 $IPAD_OUT_DIR/iPad-Icon-72@2x.png
+convert $INPUT_IMAGE -resize 76x76 $IPAD_OUT_DIR/iPad-Icon-76.png
+convert $INPUT_IMAGE -resize 152x152 $IPAD_OUT_DIR/iPad-Icon-76@2x.png
 
 # Android App Icons
 echo ==== Creating Android Icons ====
